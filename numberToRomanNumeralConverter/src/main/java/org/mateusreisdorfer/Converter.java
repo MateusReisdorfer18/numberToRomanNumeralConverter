2810abd8d13689b54083df637607046d40efe0a5
MateusReisdorfer18 <mateusreisdorfer18@gmail.com>
2025-01-24 08:20:24 -0300

    refactor

--- numberToRomanNumeralConverter/src/main/java/org/mateusreisdorfer/Converter.java
+++ numberToRomanNumeralConverter/src/main/java/org/mateusreisdorfer/Converter.java
@@ -1,6 +1,5 @@
 package org.mateusreisdorfer;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,9 +7,7 @@ import java.util.Map;
 public class Converter {
     public String numberToRomanNumeral(String value) {
         this.checkIfNumberIsValid(value);
-        char[] valueArray = value.toCharArray();
-
-        return this.buildResponse(valueArray);
+        return this.buildResponse(value.toCharArray());
     }
 
     private String buildResponse(char[] valueArray) {
@@ -27,56 +24,64 @@ public class Converter {
         for(int i=loopIndex; i<valueArray.length; i++) {
             int index = valueArray.length-1;
 
+            if(valueArray.length == 1) {
+                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(2)));
+                continue;
+            }
+
+            if(valueArray.length == 2) {
+                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(index+i)));
+                continue;
+            }
+
+            if(valueArray.length == 3) {
+                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(i)));
+                continue;
+            }
+
             if(valueArray.length == 4)
                 this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(i-1)));
-
-            if(valueArray.length == 3)
-                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(i)));
-
-            if(valueArray.length == 2)
-                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(index+i)));
-
-            if(valueArray.length == 1)
-                this.convert(valueArray[i], romanNumeral, romanNumeralMap.get(romanNumeralMapKeys.get(romanNumeralMapKeys.size()-1)));
 
         }
 
         return romanNumeral.toString();
     }
 
-    private Map<String, List<String>> getRomanNumeralMap() {
-        Map<String, List<String>> romanNumerlMap = new HashMap<>();
-        romanNumerlMap.put("hundredNumeral", List.of("C", "CD", "D", "CM"));
-        romanNumerlMap.put("tenNumeral", List.of("X", "XL", "L", "XC"));
-        romanNumerlMap.put("unityNumeral", List.of("I", "IV", "V", "IX"));
-
-        return romanNumerlMap;
-    }
-
     private void convert(char value, StringBuilder romanNumeral, List<String> listRomanNumeral) {
-        int newValue = Integer.parseInt(String.valueOf(value));
+        int newValue = Character.getNumericValue(value);
 
-        if(newValue < 4)
+        if(newValue < 4) {
             romanNumeral.append(listRomanNumeral.get(0).repeat(newValue));
+            return;
+        }
 
-        if(newValue == 4)
+        if(newValue == 4) {
             romanNumeral.append(listRomanNumeral.get(1));
+            return;
+        }
 
-        if(newValue == 5)
+        if(newValue == 5) {
             romanNumeral.append(listRomanNumeral.get(2));
+            return;
+        }
 
-        if(newValue > 5 && newValue < 9) {
+        if(newValue < 9) {
             romanNumeral.append(listRomanNumeral.get(2));
-
-            switch (newValue) {
-                case 6 -> romanNumeral.append(listRomanNumeral.get(0));
-                case 7 -> romanNumeral.append(listRomanNumeral.get(0).repeat(2));
-                case 8 -> romanNumeral.append(listRomanNumeral.get(0).repeat(3));
-            }
+            romanNumeral.append(listRomanNumeral.get(0).repeat(newValue - 5));
+            return;
         }
 
         if(newValue == 9)
             romanNumeral.append(listRomanNumeral.get(3));
+    }
+
+    private Map<String, List<String>> getRomanNumeralMap() {
+        Map<String, List<String>> romanNumeralMap = new HashMap<>();
+        romanNumeralMap.put("hundredNumeral", List.of("C", "CD", "D", "CM"));
+        romanNumeralMap.put("tenNumeral", List.of("X", "XL", "L", "XC"));
+        romanNumeralMap.put("unityNumeral", List.of("I", "IV", "V", "IX"));
+
+        return romanNumeralMap;
     }
 
     private void checkIfNumberIsValid(String value) {
